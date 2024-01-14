c05b3b628ca8d17870700be08e339b0547627487
GeroWeinkauf <149808563+GeroWeinkauf@users.noreply.github.com>
2024-01-14 16:27:04 +0100

    Add files via upload

--- App/datenesel.py
+++ App/datenesel.py
@@ -2,22 +2,24 @@ import csv
 import openai
 import logging
 import routes
+import prompts
 from config import api_key, model, temperature, max_length, top_p, frequency_penalty, presence_penalty
+
 # Konfiguration für den OpenAI GPT-3.5 Aufruf in config
 openai.api_key = api_key
 
 
 def generate_product_features(user_input):
 
-    line = user_input['TitlePlain'] + "// Lieferumfang: " + " / ".join(user_input["DeliveryContents"]) + " // " + " / ".join(user_input["UserInstructions"]) + " // "
+    input = user_input['TitlePlain'] + "// Lieferumfang: " + " / ".join(user_input["DeliveryContents"]) + " // " + " / ".join(user_input["UserInstructions"]) + " // "
 
-    system_prompt = "Du bist ein Spezialist für E-Commerce und SEO. Du hast Expertise im Extrahieren und Abstrahieren von Produktmerkmalen, welche entscheidend für die Kaufentscheidung sind, welche helfen, die Vorzüge eines Produktes zu erkennen, und welche die Benutzererfahrung hervorheben. Du arbeitest marketing- und zielgruppenorientiert. Du antwortest immer in folgendem Format: 'Produktmerkmal 1 / Produktmerkmal 2 / [...] / Produktmerkmal x'."
-    user_prompt = f"Extrahiere und abstrahiere ca. 10 Produktmerkmale anhand der folgenden Produktdaten:\n\n'{line}'\n\nIdentifiziere attraktive, verkaufsfördernde Merkmale, die für diese Produktart typisch sind, ohne jedoch spezifische Funktionen oder Eigenschaften zu erfinden, die nicht im Datensatz erwähnt werden. Stelle sicher, dass die Merkmale zwar einer subjektiven, positiven Beurteilung ähneln, aber realistisch und in Übereinstimmung mit den gegebenen Produktdaten sind. Vermeide übertriebene Annahmen oder Ergänzungen, die kaum aus den gegebenen Informationen abgeleitet werden können.\n\nAntworte im Format 'Produktmerkmal 1 / Produktmerkmal 2 / ... / Produktmerkmal x'."
+    system_prompt = prompts.system_prompt_1
+    user_prompt = prompts.user_prompt_1_part1 + input + prompts.user_prompt_1_part2
 
     routes.status_global = "Prerequests started"
 
     combined_outputs = []
-    for _ in range(3):
+    for i in range(3):
         response = openai.ChatCompletion.create(
             model=model,
             messages=[
@@ -31,16 +33,19 @@ def generate_product_features(user_input):
             presence_penalty=presence_penalty
         )
         combined_outputs.append(response.choices[0].message['content'])
+        routes.status_global = "Prerequests (" + str(i) + "/4)"
 
     output = " / ".join(combined_outputs)
+
+
 
     logging.info('Datenesel I: ' + output)
 
     return output
 
 def refine_product_features(combined_outputs):
-    system_prompt = "Du bist ein Spezialist für E-Commerce und SEO. Du hast Expertise im Extrahieren und Abstrahieren von Produktmerkmalen, welche entscheidend für die Kaufentscheidung sind, welche helfen, die Vorzüge eines Produktes zu erkennen, und welche die Benutzererfahrung hervorheben. Du arbeitest marketing- und zielgruppenorientiert. Du antwortest immer in folgendem Format: 'Produktmerkmal 1 / Produktmerkmal 2 / [...] / Produktmerkmal x'."
-    user_prompt = f"Extrahiere ca. 10 Produktmerkmale aus den folgenden Produktmerkmalen:\n\n'{combined_outputs}'\n\nIdentifiziere attraktive, verkaufsfördernde Merkmale, die für diese Produktart typisch sind. Stelle sicher, dass die Merkmale einer subjektiven, positiven Beurteilung ähneln, wie z.B. 'stilvolles Design', 'hoher Komfort' oder 'benutzerfreundliche Handhabung'. Entferne bzw. ignoriere Merkmale, die objektiverer Natur sind und sich oft in Zahlen ausdrücken, wie z.B. '3m breit' oder '40 Stück in rot und blau'. Unerwünschte Merkmale sind hier oft solche, die sich auf einem Produktdatenblatt wiederfinden würden. Du kannst objektive, spezifische Produktdetails durch beschreibende Adjektive ersetzen, die die Qualität, Funktionalität oder das Erscheinungsbild des Produkts beschreiben, wie z.B. 'leicht', 'robust' oder 'elegant'. Berücksichtige die Zielgruppe und den vorgesehenen Verwendungszweck des Produkts bei der Auswahl der Merkmale, wenn deutlich wird, um was für ein Produkt es sich handelt. Wähle Merkmale, die für potenzielle Käufer am ansprechendsten und relevantesten sind. Vermeide sehr übertriebene Aussagen.\n\nAntworte im Format 'Produktmerkmal 1 / Produktmerkmal 2 / ... / Produktmerkmal x'."
+    system_prompt = prompts.system_prompt_2
+    user_prompt = prompts.user_prompt_2_part1 + combined_outputs + prompts.user_prompt_2_part2
 
     response = openai.ChatCompletion.create(
         model=model,
@@ -55,7 +60,7 @@ def refine_product_features(combined_outputs):
         presence_penalty=presence_penalty
     )
     logging.info('Datenesel II (Finished): ' + response.choices[0].message['content'])
-    routes.status_global = "Prerequests finished"
+    routes.status_global = "Prerequests (4/4)"
 
     return response.choices[0].message['content']
 
