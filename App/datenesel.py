0e80f6e64d83c8d44abe9d3f3948fcfa7c943bb6
angeluul <104163180+angeluul@users.noreply.github.com>
2024-01-25 17:21:03 +0100

    Update datenesel.py

--- App/datenesel.py
+++ App/datenesel.py
@@ -22,7 +22,7 @@ def generate_product_features(user_input):
         logging.info('LLAMA ist noch nicht angebunden')                 #Killswitch nicht implemented yet
         exit(0)
 
-    input = user_input["product_information"]["TitlePlain"] + "// Lieferumfang: " + " / ".join(user_input["product_information"]["DeliveryContents"]) + " // " + " / ".join(user_input["product_information"]["UserInstructions"]) + " // "
+    input = user_input["product_information"]["TitlePlain"] + " // Lieferumfang: " + " / ".join(user_input["product_information"]["DeliveryContents"]) + " // " + " / ".join(user_input["product_information"]["UserInstructions"]) + " // "
 
     routes.status_global["Daten vorverarbeitet"] = True                 # status update
 
