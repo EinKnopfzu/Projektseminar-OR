0bcdc167802eacd5e1649731a2374294468cfdbf
angeluul <104163180+angeluul@users.noreply.github.com>
2024-01-25 10:08:18 +0100

    Update get_embedding.py

--- App/get_embedding.py
+++ App/get_embedding.py
@@ -38,6 +38,8 @@ def embedding(datenesel, typ_key):
     for index in range(0,3):
         if typ_key == 'AmazonBulletPoints':
             result.append("<ul><li>" + persistant_dataframe_embedding["AmazonBulletPoint1"][index] + "</li><li>" + persistant_dataframe_embedding["AmazonBulletPoint2"][index] + "</li><li>" + persistant_dataframe_embedding["AmazonBulletPoint3"][index] + "</li><li>" + persistant_dataframe_embedding["AmazonBulletPoint4"][index] + "</li><li>" + persistant_dataframe_embedding["AmazonBulletPoint5"][index] + "</li></ul>")
+        elif typ_key == 'SalesArgument':
+            return persistant_dataframe_embedding[['Inputdata', typ_key]].head(3)
         else:
             result.append(persistant_dataframe_embedding[typ_key][index])
 
