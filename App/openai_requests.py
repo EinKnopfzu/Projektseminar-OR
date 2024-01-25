fdc3a92fed95fc5527e181e78d1d690c7540b161
angeluul <104163180+angeluul@users.noreply.github.com>
2024-01-25 10:08:22 +0100

    Update openai_requests.py

--- App/openai_requests.py
+++ App/openai_requests.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 import json
 import logging
 import openai
@@ -6,6 +7,7 @@ import routes
 import prompts
 from config import api_key, model, temperature, max_length, top_p, frequency_penalty, presence_penalty
 from get_embedding import embedding
+from check_structures import check_structure
 # Konfiguration für den OpenAI GPT-3.5 Aufruf in config
 openai.api_key = api_key
 
@@ -26,26 +28,53 @@ def openai_requests(datenesel, config):
         if (config["generate_selection"][key] == True):
             routes.status_global["Hauptabfrage " + key] = True  # status update
 
-            system_prompt = prompts.hauptprompts["system_" + key]
-            user_prompt = prompts.hauptprompts["user_" + key] + datenesel
+            if key != 'SalesArgument':
+                system_prompt = prompts.hauptprompts["system_" + key]
+                user_prompt = prompts.hauptprompts["user_" + key] + datenesel
 
-            if key != 'MetaKeywordShop':
-                user_prompt += embedding(datenesel, key)
+                if key != 'MetaKeywordShop':
+                    user_prompt += embedding(datenesel, key)
 
 
-            response = openai.ChatCompletion.create(
-                model=model,
-                messages=[
-                    {"role": "system", "content": system_prompt},
-                    {"role": "user", "content": user_prompt}
-                ],
-                temperature=temperature,
-                max_tokens=max_length,
-                top_p=top_p,
-                frequency_penalty=frequency_penalty,
-                presence_penalty=presence_penalty
-            )
-
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=[
+                        {"role": "system", "content": system_prompt},
+                        {"role": "user", "content": user_prompt}
+                    ],
+                    temperature=temperature,
+                    max_tokens=max_length,
+                    top_p=top_p,
+                    frequency_penalty=frequency_penalty,
+                    presence_penalty=presence_penalty
+                )
+            else:
+                system_prompt = prompts.hauptprompts["system_" + key]
+                user_prompt = datenesel
+                dialogue = embedding(datenesel, key)
+                for i in range(3):
+                    response = openai.ChatCompletion.create(
+                        model=model,
+                        messages=[
+                            {"role": "system", "content": system_prompt},
+                            {"role": "user", "content": dialogue.iloc[0]['Inputdata']},                        
+                            {"role": "assistant", "content": dialogue.iloc[0][key]},
+                            {"role": "user", "content": dialogue.iloc[1]['Inputdata']},       
+                            {"role": "assistant", "content": dialogue.iloc[1][key]},
+                            {"role": "user", "content": dialogue.iloc[2]['Inputdata']},       
+                            {"role": "assistant", "content": dialogue.iloc[2][key]},
+                            {"role": "user", "content": user_prompt}      
+                        ],
+                        temperature=temperature,
+                        max_tokens=max_length,
+                        top_p=top_p,
+                        frequency_penalty=frequency_penalty,
+                        presence_penalty=presence_penalty
+                    )
+                    response_try = response.choices[0].message['content']
+                    if check_structure(key, response_try):
+                        break
+            
             logging.info('Hauptabfrage ' + key + ': ' + response.choices[0].message['content'])
 
             outputs.append({"typ" : key,
