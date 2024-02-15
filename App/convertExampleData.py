fe818d7ea8da139f3588a368f326cdf89409ca20
GeroWeinkauf <gero.weinkauf@student.uni-halle.de>
2024-02-15 18:38:00 +0100

    logs folder added

--- App/convertExampleData.py
+++ App/convertExampleData.py
@@ -13,12 +13,12 @@ with open('Beispieldaten.csv', newline='\n', encoding="utf8") as csvfile:
         df.at[row[0], row[1]] = row[2]
 
 # set index name
-df.index.name = 'ID'
+df.index.name = 'ID' 
 # change order of some columns
 col_order = ['TitlePlain', 'TitleShop', 'TitleAmazon', 'TitleLimango', 'TitleEbay', 'EbayTitleDeluxe', 'DescriptionLongMarketplaces', 'DescriptionLongShops']
 for id, col in enumerate(col_order):
     df.insert(id, col, df.pop(col))
 print(df)
 
-# export dataframe to csv
+#export dataframe to csv
 df.to_csv('Beispieldaten_convertiert.csv', sep=',', encoding='utf-8')
